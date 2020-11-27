45fb2aeb55cfbcb9b1af9ed4bd6de33fb3646f44
Marek Szuba <mszuba01@gmail.com>
2020-11-27 23:15:32 +0100

    add posts create/update actions

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -10,3 +10,23 @@ export const getPosts = () => async (dispatch) => {
     console.log(err.message);
   }
 };
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: 'CREATE', payload: data });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: 'UPDATE', payload: data });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
